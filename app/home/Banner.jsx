7e6bc5c030f19276111df57f5b953e59e2e73993
khaled6013 <kmd601384@gmail.com>
2025-11-13 18:22:58 +0600

    banner part done

--- app/home/Banner.jsx
+++ app/home/Banner.jsx
@@ -1,62 +1,87 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 const Banner = () => {
   return (
     <>
-      <div className="py-2 bg-[#010101]">
-        <div className="lg:w-10/12 w-11/12 mx-auto px-10 relative">
-          <div className="">
-            <h2 className='text-center font-bold lg:text-[190px] text-[60px] text-white '>Cyber Punk</h2>
+      <div className="relative bg-[#010101] py-10 overflow-visible">
+        <div className="w-11/12 lg:w-10/12 mx-auto relative px-14">
+          <h2 className="text-center font-bold text-white lg:text-[190px] text-[60px] leading-[0.8]">
+            Cyber Punk
+          </h2>
+          <img
+            src="/image/banOne.png"
+            alt="Cyber Hero"
+            className="absolute top-20 left-1/2 -translate-x-1/2 w-[780px] h-[700px] object-cover z-0"
+          />
+        </div>
+        <div className="w-11/12 lg:w-10/12 mx-auto mt-[180px] relative flex justify-between items-start z-20 px-14">
+          <div>
+            <h3 className="font-semibold text-white text-[24px]">Stories & Lore</h3>
+            <p className="font-medium text-[#DDDDDD] pt-3">
+              Dive into compelling narratives <br /> set in a dystopian future.
+            </p>
           </div>
-          <div className="flex items-center justify-between mt-[120px]">
-            <div className="">
-              <h3 className='font-semibold text-white text-[24px] '>Stories & Lore</h3>
-              <p className='font-medium text-[#DDDDDD] pt-3'>Dive into compelling narratives <br></br> set in a dystopian future.</p>
-            </div>
-            <div className="absolute top-29 left-[30%]">
-              <img src="/image/banOne.png" alt="banOne" className='w-[750px] h-[700px]' />
-            </div>
-            <div className="">
-              <h2 className='font-normal text-[36px] Uppercase text-white text-end'>Events and</h2>
-              <h2 className='font-semibold text-[36px] uppercase text-white text-end'>Updates</h2>
-              <p className='font-medium text-[#DDDDDD] text-[18px] pt-11 text-end'>Explore the high-tech, low-life world <br></br> where the lines between humanity<br></br> and machinery blur.</p>
-              <div className="text-end mt-13">
-                <a className='py-3 px-6 text-white text-[16px] font-semibold bg-linear-to-r from-[#F0B71F] to-[#E03609] cursor-pointer rounded-lg'>Read More</a>
+          <div className="text-right">
+            <h2 className="font-normal text-[36px] uppercase text-white">Events and</h2>
+            <h2 className="font-semibold text-[36px] uppercase text-white">Updates</h2>
+            <p className="font-medium text-[#DDDDDD] text-[18px] pt-7">
+              Explore the high-tech, low-life world <br />
+              where the lines between humanity <br />
+              and machinery blur.
+            </p>
+
+            <button className="mt-10 py-3 px-6 text-white text-[16px] font-semibold bg-linear-to-r from-[#F0B71F] to-[#E03609] rounded-lg">
+              Read More
+            </button>
+          </div>
+        </div>
+        <div className="w-11/12 lg:w-10/12 mx-auto mt-[100px] flex justify-between items-start px-14">
+          <div className="px-8 py-6 backdrop-blur-[51px] bg-white/10 rounded-xl border border-[#E03609]/50">
+            <div className="flex items-center">
+              <img src="/image/banTwo.png" alt="Card Image" className="w-[90px]" />
+
+              <div className="ml-6">
+                <h2 className="font-semibold text-[56px] text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]">
+                  76,285K+
+                </h2>
+                <h4 className="font-normal text-[28px] text-white">Experience the Future</h4>
+                <p className="font-medium text-[#DDDDDD] pt-3">
+                  Explore the high-tech, low-life world <br />
+                  where the lines between
+                </p>
               </div>
             </div>
           </div>
-          <div className="py-2 bg-black w-full">
-            <div className="flex justify-between items-center">
-              <div className="px-8 py-6 backdrop-blur-[51.4px] bg-white/10 rounded-xl border-amber-700 border-2">
-                <div className="flex  items-center">
-                  <div className="">
-                    <img src="/image/banTwo.png" alt="banTwo" />
-                  </div>
-                  <div className="ml-6">
-                    <h2 className='font-semibold text-[56px] text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]'>76,285K+</h2>
-                    <h4 className='font-normal text-[28px] text-white'>Experience the Future</h4>
-                    <p className='font-medium text-[#DDDDDD] pt-3'>Explore the high-tech, low-life world<br></br> where the lines between</p>
-                  </div>
-                </div>
-              </div>
-              <div className="">
-                <h2 className='text-[36px] font-normal text-white'>Art and</h2>
-                <h2 className='text-[36px] font-semibold text-white'> Design</h2>
-                <p className='font-medium text-[#DDDDDD] pt-8'>Feast your eyes on stunning<br>
-                </br> visuals and concept art that<br>
-                  </br> bring the cyberpunk</p>
-              </div>
-              <div className="p-6 backdrop-blur-[51.4px] bg-white/10 rounded-xl border-amber-700 border-2">
-                <h2 className='font-semibold text-[56px] text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]'>17+</h2>
-                <h4 className='font-medium text-white text-[27px]'>Years of<br></br> Experiences</h4>
-              </div>
-            </div>
+          <div className="mt-10">
+            <h2 className="text-[36px] font-normal text-white uppercase">Art and</h2>
+            <h2 className="text-[36px] font-semibold text-white uppercase">Design</h2>
+            <p className="font-medium text-[#DDDDDD] pt-8">
+              Feast your eyes on stunning <br />
+              visuals and concept art that <br />
+              bring the cyberpunk
+            </p>
           </div>
+          <div className="px-8 py-6 backdrop-blur-[51px] bg-white/10 rounded-xl border border-[#E03609]/50 mt-10">
+            <h2 className="font-semibold text-[56px] text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]">
+              17+
+            </h2>
+            <h4 className="font-medium text-white text-[27px] leading-tight">
+              Years of <br /> Experiences
+            </h4>
+          </div>
+        </div>
+        <div className="absolute top-0 left-35 h-full w-0.5 bg-linear-to-b from-[#E03609] to-[#F0B71F]"></div>
+        <div className="absolute top-0 right-35 h-full w-0.5 bg-linear-to-b from-[#F0B71F] to-[#E03609]"></div>
+        <div className="absolute -top-8 right-[110px]">
+          <img src="/image/u.png" alt="u" />
+        </div>
+        <div className="absolute -top-8 left-[110px]">
+          <img src="/image/ut.png" alt="u" />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
--- app/home/Nav.jsx
+++ app/home/Nav.jsx
@@ -4,9 +4,9 @@ import React, { useState } from 'react'
 const Nav = () => {
     let [open, setOpen] = useState(false)
     return (
-        <div className="bg-black py-10 ">
-            <div className="w-10/12 mx-auto px-10">
-                <div className="lg:flex items-center justify-between">
+        <div className="bg-black pt-10 relative">
+            <div className="w-10/12 mx-auto px-14 ">
+                <div className="lg:flex items-center justify-between ">
                     <div className="">
                         <img src="/image/logo.png" alt="logo" />
                     </div>
@@ -30,10 +30,10 @@ const Nav = () => {
                         <a onClick={() => setOpen(!open)} className='text-[17px] font-bold text-white py-3 px-5 bg-linear-to-r from-[#F0B71F] to-[#E03609] cursor-pointer rounded-lg'>Get started free</a>
                     </div>
                 </div>
-                <div className="w-0.5 h-full bg-linear-to-r from-[#E03609] to-[#F0B71F] absolute top-0 left-[130px] "></div>
-                <div className="w-0.5 h-full bg-linear-to-r from-[#E03609] to-[#F0B71F] absolute top-0 right-[130px] "></div>
+                <div className="absolute top-0 left-35 h-full w-0.5 bg-linear-to-b from-[#E03609] to-[#F0B71F]"></div>
+                <div className="absolute top-0 right-35 h-full w-0.5 bg-linear-to-b from-[#F0B71F] to-[#E03609]"></div>
             </div>
-            <div className="w-full h-0.5 bg-linear-to-r from-[#E03609] to-[#F0B71F] mt-15">
+            <div className="w-full h-0.5 bg-linear-to-r from-[#E03609] to-[#F0B71F] mt-15 ">
             </div>
         </div>
     )
