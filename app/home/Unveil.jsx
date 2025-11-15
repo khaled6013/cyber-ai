a4f02d461d30220a5358de622ef14baab38a61be
khaled6013 <kmd601384@gmail.com>
2025-11-15 21:01:29 +0600

    un part

---
+++ app/home/Unveil.jsx
@@ -0,0 +1,91 @@
+import React from 'react'
+
+const Unveil = () => {
+    return (
+        <>
+            <div className="py-14 bg-black relative">
+                <div className="w-11/12 lg:w-10/12 mx-auto px-14">
+                    <div className="flex items-center justify-between gap-10">
+                        <div className="w-1/2">
+                            <div className="text-white">
+                                <h2 className="font-bold leading-tight text-[56px]">
+                                    Where{" "}
+                                    <span className="text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]">
+                                        Neon Dreams <br></br>
+                                    </span>{" "}
+                                    <span className="text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]">
+                                        & Digital
+                                    </span>{" "}
+                                    Nightmares Converge.
+                                </h2>
+                            </div>
+                            <div className="mt-[60px]">
+                                <p className='text-[18px] font-medium text-[#DDDDDD]'>A realm where advanced technology meets dystopian reality. Our website is your gateway to a universe of neon lights, gritty streets, and cybernetic enhancements. Explore the high-tech, low-life world where the lines between humanity and machinery blur.</p>
+                                <div className="flex items-center justify-between mt-[65px]">
+                                    <div className="">
+                                        <h2 className='text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F] text-[47px] font-semibold'>89,2K+</h2>
+                                        <p className='font-medium text-[17px] text-white'>Discover the Future</p>
+                                    </div>
+                                    <div className="">
+                                        <h2 className='text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F] text-[47px] font-semibold'>7001+</h2>
+                                        <p className='font-medium text-[17px] text-white'>Stories and Lore</p>
+                                    </div>
+                                    <div className="">
+                                        <h2 className='text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F] text-[47px] font-semibold'>3,124+</h2>
+                                        <p className='font-medium text-[17px] text-white'>Art and Design</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="w-1/2">
+                            <img src="/image/unImg.png" alt="unImg" />
+                        </div>
+                    </div>
+                    <div className="flex items-center  gap-10 mt-11">
+                        <div className="w-1/2">
+                            <img src="/image/unTwo.png" alt="unTwo" />
+                        </div>
+                        <div className="w-1/2">
+                            <div className="text-white">
+                                <h2 className="font-bold leading-tight text-[56px]">
+                                    Unveil{" "}
+                                    <span className="text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]">
+                                        the Secrets <br></br>
+                                    </span>{" "}
+                                    <span className="text-transparent bg-clip-text bg-linear-to-r from-[#E03609] to-[#F0B71F]">
+                                        of the
+                                    </span>{" "}
+                                    Cybernetic Underworld
+                                </h2>
+                            </div>
+                            <div className="mt-[60px]">
+                                <p className='text-[18px] font-medium text-[#DDDDDD]'>A realm where advanced technology meets dystopian reality. Our website is your gateway to a universe of neon lights, gritty streets,<br></br> and cybernetic enhancements. </p>
+                            </div>
+                            <div className="flex items-center gap-6 mt-15 border border-[#F0B71F] pl-10 pt-8 pr-13 pb-8 rounded-[20px]">
+                                <div className="">
+                                    <img src="/image/unTb.png" alt="unTb" />
+                                </div>
+                                <div className="">
+                                    <h3 className='text-white font-medium text-[28px]'>Join the Cyber Revolution</h3>
+                                    <p className='text-[#DDDDDD] font-medium text-[18px] pt-[22px]'>Explore the high-tech, low-life<br></br> world where the lines between<br></br> humanity and machinery blur.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="absolute top-0 left-35 h-full w-0.5 bg-linear-to-b from-[#E03609] to-[#F0B71F]"></div>
+                    <div className="absolute top-0 right-35 h-full w-0.5 bg-linear-to-b from-[#F0B71F] to-[#E03609]"></div>
+                    <div className="absolute h-0.5 w-full left-0 top-0 bg-linear-to-b from-[#F0B71F] to-[#E03609]"></div>
+                    <div className="absolute -top-8 right-[110px]">
+                        <img src="/image/u.png" alt="u" />
+                    </div>
+                    <div className="absolute -top-8 left-[110px]">
+                        <img src="/image/ut.png" alt="u" />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Unveil
--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Banner from "./home/Banner";
+import Unveil from "./home/Unveil";
 
 export default function Home() {
   return (
     <>
       <Banner></Banner>
+      <Unveil></Unveil>
     </>
   );
 }
